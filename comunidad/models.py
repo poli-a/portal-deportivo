e900cbcec71b3718d2a2949ef2cbb625d4144bf6
ralarcon <ralarcon@grupoaaf.com.ar>
2020-11-10 22:53:21 -0300

    Agregado app Comunidad y modelo Post

---
+++ comunidad/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from portal.models import Categoria
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('pendiente', 'Pendiente'),
+        ('publicado', 'Publicado'),
+    )
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='fecha_publicacion')
+    usuario = models.ForeignKey(User,
+                                null=True,
+                                on_delete=models.SET_NULL)
+    categoria = models.OneToOneField(Categoria,
+                                null=True,
+                                on_delete=models.CASCADE)
+    nota = models.TextField()
+    media_url = models.TextField()
+    fecha_publicacion = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                            choices=STATUS_CHOICES,
+                            default='pendiente')
+    class Meta:
+        ordering = ('-fecha_publicacion',)
+    def __str__(self):
+        return self.titulo
+
--- portal_deportivo/settings.py
+++ portal_deportivo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'portal.apps.PortalConfig',
     'competiciones.apps.CompeticionesConfig',
+    'comunidad.apps.ComunidadConfig',
 ]
 
 MIDDLEWARE = [
